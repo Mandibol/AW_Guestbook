aabc95e05b25233f698fd6e9ae55d3a277268d41
Mandibol <92530271+Mandibol@users.noreply.github.com>
2021-12-02 05:48:44 +0100

    Added: Login, post count and html escape

--- Public/JavaScript/GuestBookScript.js
+++ Public/JavaScript/GuestBookScript.js
@@ -1,40 +1,35 @@
 const user = {};
 user.name = "Guest";
 user.posts = 0;
+user.loggedIn = false;
 
 //Function to Print and refresh posts
 function refreshPosts() {
     const client = new XMLHttpRequest();
-    client.open('post', '/refresh_posts', true);
     client.onload= () => {
         let displayHTML = "";
-        const posts = JSON.parse(client.responseText);            
-        
+        const posts = JSON.parse(client.responseText);
+        user.posts = 0;            
         for (let i = 0; i < posts.length; i++) {
             //Post Layout
             const date = (posts[i].date).substring(0,10);
             const time = (posts[i].date).substring(11,16);
+            const comment = (posts[i].comment).replace(/\n/g, "<br>")
             displayHTML += '<section>'
-            displayHTML += `<h3>Användare: ${posts[i].userName}</h3>`;
-            displayHTML += `<h4>Postat: ${date} , ${time}</h4>`;
-            displayHTML += `<p>${posts[i].comment}</p>`;
+            displayHTML += `<h3>Postat av ${posts[i].userName} ${date} ${time}</h3>`;
+            displayHTML += `<p>${comment}</p>`;
             displayHTML += '</section>'
+            if (posts[i].userName === user.name) {
+            user.posts++;
+            }
         }
         document.getElementById("comments").innerHTML = displayHTML;
+        let text = `Välkommen ${user.name}! Skriv gärna en kommentar <br> Du har skrivit ${user.posts} kommentarer`;
+        document.getElementById("greet").innerHTML = text;
     };
+    client.open('post', '/refresh_posts', true);
     client.setRequestHeader("Content-type", "application/json; charset=utf-8");
     client.send();
-}
-
-//function to send form data to server
-function sendPost(formJSON){
-    const client = new XMLHttpRequest();
-    client.open('post', '/process_post', true);
-    client.onload= () => {
-        refreshPosts();
-    };
-    client.setRequestHeader("Content-type", "application/json; charset=utf-8");
-    client.send(formJSON);
 }
 
 //Wait for site to load before continue
@@ -42,25 +37,114 @@ window.onload = () => {
     //Get and print current posts
     refreshPosts();
 
+    //Get form element ids
+    const createUserForm = document.getElementById("createUserForm");
+    const createUserName = document.getElementById("createUserName");
+    const createUserEmail = document.getElementById("createUserEmail");
+    const createUserPsw = document.getElementById("createUserPsw");
+    const loginForm = document.getElementById("loginForm");
+    const loginUserName = document.getElementById("loginName");
+    const loginPsw = document.getElementById("loginPsw");
+    const commentForm = document.getElementById("commentForm");
+    const comment = document.getElementById("comment");
+
+    //Manage what sections are available when logged in
+    document.getElementById("createAccountLink").addEventListener("click", (evt) => {
+        loginForm.style.display = "none";
+        createUserForm.style.display = "block";
+    });
+    document.getElementById("returnLink").addEventListener("click", (evt) => {
+        loginForm.style.display = "block";
+        createUserForm.style.display = "none";
+    });
+    document.getElementById("logOut").addEventListener("click", (evt) => {
+        loginForm.style.display = "block";
+        commentForm.style.display = "none";
+        user.name = "Guest";
+        user.loggedIn = false;
+    });
+
     //Submit Post
-    document.getElementById("commentForm").addEventListener("submit", function(evt){
-        //Prevent Default
+    commentForm.addEventListener("submit", (evt) => {
         evt.preventDefault();
         //Collect form data
         const objForm = {};
-        objForm.userName = document.getElementById("userName").value;
-        objForm.comment = document.getElementById("comment").value;
+        objForm.userName = user.name;
+        objForm.comment = comment.value;
         objForm.date = new Date();
-        //validate form data
-
         //Convert Object to JSON String
         const formJSON = JSON.stringify(objForm);
-        console.log(formJSON);
         //Send post to server and refresh
-        sendPost(formJSON);
-
+        const client = new XMLHttpRequest();
+        client.onload= () => {
+            refreshPosts();
+        };
+        client.open('post','/process_post', true);
+        client.setRequestHeader("Content-type", "application/json; charset=utf-8");
+        client.send(formJSON);
         //Reset Form Field
-        document.getElementById("comment").value = "";
-        document.getElementById("userName").value = "";
+        comment.value = "";
+    });
+
+    //Create User
+    createUserForm.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        //Collect form data
+        const createUser = {};
+        createUser.name = createUserName.value;
+        createUser.email = createUserEmail.value;
+        createUser.psw = createUserPsw.value;
+        //Convert Object to JSON String
+        const formJSON = JSON.stringify(createUser);
+        //Send createUser request to server
+        const client = new XMLHttpRequest();
+        client.onload= () => {
+            const response = JSON.parse(client.responseText);
+            //toggle login form
+            if (response.success === true) {
+                loginForm.style.display = "block";
+                createUserForm.style.display = "none";
+            }
+            alert(response.alert)
+        };
+        client.open("post","/create_user", true);
+        client.setRequestHeader("Content-type", "application/json; charset=utf-8");
+        client.send(formJSON);
+        //Reset Form Field
+        createUserName.value = "";
+        createUserEmail.value = "";
+        createUserPsw.value = "";
+    });
+
+    //Login
+    loginForm.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        //Collect form data
+        const login = {};
+        login.name = loginUserName.value;
+        login.psw = loginPsw.value;
+        //Convert Object to JSON String
+        const formJSON = JSON.stringify(login);
+        // Send login request to server
+        const client = new XMLHttpRequest();
+        client.onload= () => {
+            const response = JSON.parse(client.responseText);
+            if (response.login){
+                user.name = response.name;
+                user.loggedIn = response.login;
+                loginForm.style.display = "none";
+                commentForm.style.display = "block";
+                refreshPosts();
+            }
+            else{
+                alert(response.alert);
+            } 
+        };
+        client.open("post","/login", true);
+        client.setRequestHeader("Content-type", "application/json; charset=utf-8");
+        client.send(formJSON);
+        //Reset Form Field
+        loginUserName.value = "";
+        loginPsw.value = "";
     });
 };
--- index.js
+++ index.js
@@ -10,31 +10,112 @@ app.listen(3000);
 console.log("Kör servern på localhost:3000");
 
 
-const postsPath = 'public/posts.txt';
+const postsPath = 'posts.json';
 
-//Initiate posts array and load it from file
-let posts = [];
-fs.readFile(postsPath, (err,data) =>{
-    if (err) throw err;
-    posts = JSON.parse(data);
-});
+//Functions to escape bad html
+function escapeHTML(str) {
+    return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+}
+function escapeObj(obj) {
+    const tempObj = obj;
+    for (let x in obj) {
+        tempObj[x] = escapeHTML(obj[x]);
+    };
+    return tempObj;
+}
 
 //redirect to frontpage
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-//
+//Manage comment request
 app.post("/process_post",(req,res) => {
-    //Lägg till kommentaren till array comments
-    posts.push(req.body);
-    //Ersätt och Uppdatera comments.txt
-    fs.writeFile(postsPath, JSON.stringify(posts), (err) => {
-         if (err) throw err;
+    fs.readFile(postsPath, (err,data) =>{
+        if (err) throw err;
+        //Create Array from data
+        let posts = JSON.parse(data);
+        //Escape bad html chars
+        let reqTemp = escapeObj(req.body);
+        //Add comment to array
+        posts.push(reqTemp);
+        //Replace posts.json with updated array
+        fs.writeFile(postsPath, JSON.stringify(posts), (err) => {
+            if (err) throw err;
+            res.end();
+        });
     });
-    res.end();
  });
 
+ //Refresh Comment field
 app.post("/refresh_posts", (req,res) => {
-     res.send(posts);
+    fs.readFile(postsPath, (err,data) =>{
+        if (err) throw err;
+        let posts = JSON.parse(data);
+        res.send(posts);
+    });
+});
+
+//Create new user
+app.post("/create_user", (req,res) => {
+    //Escape Bad html
+    let newUser = escapeObj(req.body);
+    fs.readFile('users.json', (err,data) =>{
+        if (err) throw err;
+        const users = JSON.parse(data);
+        //Check against existing user if name or email is in use
+        const checkName = users.find(obj => obj.name === newUser.name);
+        const checkEmail = users.find(obj => obj.email === newUser.email);
+
+        let answer = {};
+        if (checkEmail === undefined && checkName === undefined) {
+            answer.alert = "Grattis ditt konto är skapat. Du kan nu logga in";
+            answer.success = true;
+            users.push(newUser);
+            fs.writeFile('users.json', JSON.stringify(users), (err) => {
+                if (err) throw err;
+            });
+        }
+        else if (checkName !== undefined && checkEmail !== undefined){
+            answer.alert = "Ånej Användarnamnet är upptaget\nDet finns redan ett konto med den mejladdressen";
+            answer.success = false;
+        }
+        else if (checkEmail !== undefined){
+            answer.alert = "Det finns redan ett konto med den mejladdressen";
+            answer.success = false;
+        }
+        else if (checkName !== undefined){
+            answer.alert = "Ånej Användarnamnet är upptaget";
+            answer.success = false;
+        }
+        res.send(JSON.stringify(answer));
+    });
+});
+
+//Login
+app.post("/login", (req,res) => {
+    let login = escapeObj(req.body);
+    fs.readFile('users.json', (err,data) =>{
+        if (err) throw err;
+        const users = JSON.parse(data);
+        //Check against existing user if name and psw match
+        const checkName = users.find(obj => obj.name === login.name);
+        let answer = {};
+
+        if (checkName !== undefined){
+            if (checkName.psw === login.psw){
+                answer.name = checkName.name;
+                answer.login = true;
+            }
+            else {
+                answer.alert = "Fel Lösenord"
+                answer.login = false;
+            }
+        }
+        else {
+            answer.alert = "Finns ingen användare med det namnet"
+            answer.login = false;
+        }
+        res.send(JSON.stringify(answer));
+    });
 });
